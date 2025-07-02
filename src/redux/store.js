03133c9abc38289830d46f45b57e51183f79202d
Baxiboo <4avramenko.yana@gmail.com>
2025-07-02 19:07:15 +0300

    first try. added code for 2 components - TransactionsChart and TransactionsTotalAmount, + their stylization (not figma for now). added code for transactions logic with redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-export const store = configureStore({
-  reducer: {},
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';   
+import transactionsReducer from './transactions/slice'; 
+ 
+ 
+export const store = configureStore({ 
+  reducer: { 
+    transactions: transactionsReducer,  
+  }, 
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }), 
+  devTools: process.env.NODE_ENV === 'development',
 });
+ 
+export const persistor = persistStore(store);
