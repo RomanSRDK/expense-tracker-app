e176ecf9112c2b8067de876042442eaebbda65c4
Baxiboo <4avramenko.yana@gmail.com>
2025-07-03 16:25:12 +0300

    added logo for both type of users

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,39 +1,25 @@
-// src/components/Logo/Logo.jsx
+ 
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+ 
+import { selectIsLoggedIn } from '../../redux/auth/selectors'; 
+import logoIconUrl from '../../../public/ExpenseTracker.svg'; 
 
-// Імпортуємо селектор, щоб дізнатись статус користувача
-import { selectIsLoggedIn } from '../../redux/auth/selectors'; // Шлях може відрізнятись
-
-// Імпортуємо ваші файли логотипів
-import logoDesktop from '../../assets/logo-desktop.svg'; // Лого для десктопу/планшету
-import logoMobile from '../../assets/logo-mobile.svg';   // Лого для мобілки
-
-// Імпортуємо стилі
 import styles from './Logo.module.css';
 
 const Logo = () => {
-  // Отримуємо статус авторизації з Redux store
   const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  // Визначаємо, куди буде вести посилання
-  // Якщо користувач залогінений - на головну сторінку транзакцій, якщо ні - на сторінку привітання
   const destination = isLoggedIn ? '/transactions/expenses' : '/';
 
   return (
-    <Link to={destination} className={styles.logoLink}>
-      <picture>
-        {/* Це джерело буде використовуватись, якщо ширина екрана 768px або більше */}
-        <source media="(min-width: 768px)" srcSet={logoDesktop} />
-        
-        {/* Це зображення буде використовуватись за замовчуванням (для мобільних) */}
-        <img
-          src={logoMobile}
-          alt="Expense Tracker Logo"
-          className={styles.logoImage}
-        />
-      </picture>
+     <Link to={destination} className={styles.logoLink}> 
+      <img
+        src={logoIconUrl}
+        alt="Expense Tracker Icon"
+        className={styles.logoIcon}
+      />
+      <span className={styles.logoText}>EXPENSETRACKER</span>
     </Link>
   );
 };
