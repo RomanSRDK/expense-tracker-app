e1e37586083fe0b87f76b39afae33a80d4f32e4b
Roman Serdiuk <roman.srdk21@gmail.com>
2025-07-03 06:20:28 -0400

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
-import { store } from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 
 import "@fontsource-variable/inter";
