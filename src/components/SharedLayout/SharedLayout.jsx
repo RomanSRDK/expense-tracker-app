9da250b3325a5a55263f34e2bc4c3b630824656c
ivannakm <ivadatalover@gmail.com>
2025-07-03 14:20:16 +0200

    add sharedLayout route

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import BgImageWrapper from "../BgImageWrapper/BgImageWrapper";
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
-const SharedLayout = ({ children }) => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
+const SharedLayout = ({ isLoggedIn }) => {
   return (
     <>
       <Header />
