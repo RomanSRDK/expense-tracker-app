c56cce2271c30081fbe49bff20f1e32169a24f42
Roman Serdiuk <roman.srdk21@gmail.com>
2025-07-03 10:44:53 -0400

    .

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,11 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+
+import "@fontsource-variable/inter";
+import "modern-normalize/modern-normalize.css";
 import "./index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
