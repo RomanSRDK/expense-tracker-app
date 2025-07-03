eae476c042070ce02fc07311bdab706f81443b1b
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-07-02 21:51:26 -0600

    add PersistGate; change App with isRefreshing; updatethe store and slice

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -13,7 +13,7 @@ const setAuthHeader = (token) => {
 };
 
 /*
- * POST @ /users/signup
+ * POST @ /auth/register
  * body: { name, email, password }
  */
 export const register = createAsyncThunk(
@@ -32,17 +32,45 @@ export const register = createAsyncThunk(
 );
 
 /*
- * POST @ /users/login
+ * POST @ /auth/login
  * body: { email, password }
  */
 export const logIn = createAsyncThunk(
   "auth/login",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("auth/login", credentials);
+      const res = await axios.post("/auth/login", credentials);
       // After successful login, add the token to the HTTP header
-      console.log(" Server response:", res.data);
       setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+/*
+ * GET @ /auth/refresh
+ * headers: Authorization: Bearer token
+ */
+export const refreshUser = createAsyncThunk(
+  "auth/refresh",
+  async (_, thunkAPI) => {
+    // Reading the token from the state via getState()
+    const state = thunkAPI.getState();
+    const refreshToken = state.auth.refreshToken;
+    const sid = state.auth.sid;
+
+    if (!refreshToken || !sid) {
+      // If there is no token, exit without performing any request
+      return thunkAPI.rejectWithValue("No session info for refresh");
+    }
+
+    try {
+      // If there is a token, add it to the HTTP header and perform the request
+      setAuthHeader(refreshToken);
+      const res = await axios.post("/auth/refresh", { sid });
+
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 // import { register, logIn, logOut, refreshUser } from "./operations";
-import { register, logIn } from "./operations";
+import { register, logIn, refreshUser } from "./operations";
 
 const authSlice = createSlice({
   name: "auth",
@@ -14,6 +14,7 @@ const authSlice = createSlice({
       transactionsTotal: { incomes: 0, expenses: 0 },
     },
     token: null,
+    sid: null,
     refreshToken: null,
     isLoggedIn: false,
     isRefreshing: false,
@@ -24,30 +25,36 @@ const authSlice = createSlice({
         state.user = action.payload.user;
         state.token = action.payload.accessToken;
         state.refreshToken = action.payload.refreshToken;
+        state.sid = action.payload.sid;
         state.isLoggedIn = true;
       })
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.accessToken;
         state.refreshToken = action.payload.refreshToken;
+        state.sid = action.payload.sid;
         state.isLoggedIn = true;
+      })
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.accessToken;
+        state.refreshToken = action.payload.refreshToken;
+        state.sid = action.payload.sid;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state) => {
+        state.isRefreshing = false;
       });
     //   .addCase(logOut.fulfilled, (state) => {
     //     state.user = { name: null, email: null };
     //     state.token = null;
     //     state.isLoggedIn = false;
     //   })
-    //   .addCase(refreshUser.pending, (state) => {
-    //     state.isRefreshing = true;
-    //   })
-    //   .addCase(refreshUser.fulfilled, (state, action) => {
-    //     state.user = action.payload;
-    //     state.isLoggedIn = true;
-    //     state.isRefreshing = false;
-    //   })
-    //   .addCase(refreshUser.rejected, (state) => {
-    //     state.isRefreshing = false;
-    //   });
+    //
   },
 });
 
