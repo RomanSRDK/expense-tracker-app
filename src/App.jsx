efb772c6ed70fae2e7e02d3e8e5b9fd022c0cf72
ivannakm <ivadatalover@gmail.com>
2025-07-03 14:24:36 +0200

    add sharedLayout route + BgImageWrapper (#11)
    * add sharedLayout
    
    * css-authnav
    
    * Update App.jsx
    
    * add sharedLayout route

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense, useEffect } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { selectIsRefreshing } from "./redux/auth/selectors";
+import { selectIsLoggedIn, selectIsRefreshing } from "./redux/auth/selectors";
 import { refreshUser } from "./redux/auth/operations";
+import SharedLayout from "./components/SharedLayout/SharedLayout";
 
 const WelcomePage = lazy(() => import("./pages/WelcomePage/WelcomePage"));
 const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
@@ -19,6 +20,7 @@ const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 function App() {
   const dispatch = useDispatch();
   const isRefreshing = useSelector(selectIsRefreshing);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   useEffect(() => {
     dispatch(refreshUser());
@@ -30,9 +32,11 @@ function App() {
       <>
         <Suspense fallback={null}>
           <Routes>
-            <Route path="/" element={<WelcomePage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route isLoggedIn={isLoggedIn} element={<SharedLayout />}>
+              <Route index element={<WelcomePage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Route>
             <Route
               path="/transactions/:transactionsType"
               element={<MainTransactionsPage />}
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import BgImageWrapper from "../BgImageWrapper/BgImageWrapper";
+
+const SharedLayout = ({ isLoggedIn }) => {
+  return (
+    <>
+      <Header />
+      {!isLoggedIn && <BgImageWrapper />}
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default SharedLayout;
