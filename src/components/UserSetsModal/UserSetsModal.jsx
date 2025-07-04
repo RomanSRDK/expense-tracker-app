dcd93c496fc53f73fc0682daa102e26d3f99c806
Roman Serdiuk <100283300+RomanSRDK@users.noreply.github.com>
2025-07-04 08:11:14 -0400

    User sets modal (#25)
    * .
    
    * redux_USER

--- src/components/UserSetsModal/UserSetsModal.jsx
+++ src/components/UserSetsModal/UserSetsModal.jsx
@@ -1,5 +1,14 @@
+import { createPortal } from "react-dom";
+import css from "./UserSetsModal.module.css";
+
 function UserSetsModal() {
-  return <div>UserSetsModal</div>;
+  return createPortal(
+    <div className={css.backdrop}>
+      <div className={css.modal}>
+        <h2>Profile settings</h2>
+      </div>
+    </div>
+  );
 }
 
 export default UserSetsModal;
