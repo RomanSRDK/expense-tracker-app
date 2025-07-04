1d317c9ecabab695ef3644315ff188b7e26d9e09
ivannakm <ivadatalover@gmail.com>
2025-07-04 16:15:40 +0200

    add position sharedLayout

--- src/components/BgImageWrapper/BgImageWrapper.jsx
+++ src/components/BgImageWrapper/BgImageWrapper.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import DecorationTab from "../DecorationTab/DecorationTab";
 import css from "./BgImageWrapper.module.css";
 import { useLocation } from "react-router-dom";
@@ -5,11 +6,26 @@ import { useLocation } from "react-router-dom";
 const BgImageWrapper = ({ children }) => {
   const location = useLocation();
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isWelcomePage = location.pathname === "/";
+  const isDesktop = windowWidth >= 1440;
+
+  if (!isDesktop && !isWelcomePage) {
+    return null;
+  }
+
   // Render nothing on mobile/tablet if NOT WelcomePage
   // Always render on desktop (CSS controls visibility)
-  if (location.pathname !== "/" && window.innerWidth < 1440) {
-    return <>{children}</>;
-  }
+  // if (location.pathname !== "/" && window.innerWidth < 1440) {
+  //   return <>{children}</>;
+  // }
   return (
     <div className={css.bgWrapper}>
       <DecorationTab />
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../Header/Header";
 import BgImageWrapper from "../BgImageWrapper/BgImageWrapper";
+import s from "./SharedLayout.module.css";
 
 const SharedLayout = ({ isLoggedIn }) => {
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === "/login" ||
+    location.pathname === "/register" ||
+    location.pathname === "/";
   return (
     <>
       <Header />
-      {!isLoggedIn && <BgImageWrapper />}
-      <main>
-        <Outlet />
-      </main>
+      <div className={`${s.layoutWrapper} ${isAuthPage ? s.authLayout : ""}`}>
+        {!isLoggedIn && (
+          <div className={s.imageContainer}>
+            <BgImageWrapper />
+          </div>
+        )}
+        <main className={s.main}>
+          <Outlet />
+        </main>
+      </div>
     </>
   );
 };
