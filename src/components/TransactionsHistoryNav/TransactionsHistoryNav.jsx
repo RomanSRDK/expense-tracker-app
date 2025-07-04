5f5e73cb4c0596ec3e845a1bcb2defbaf7397f9f
Avramenko Yana <4avramenko.yana@gmail.com>
2025-07-05 01:53:40 +0300

    finished css + added MainTransactionsHeader (#29)
    Co-authored-by: Roman Serdiuk <100283300+RomanSRDK@users.noreply.github.com>

--- src/components/TransactionsHistoryNav/TransactionsHistoryNav.jsx
+++ src/components/TransactionsHistoryNav/TransactionsHistoryNav.jsx
@@ -1,5 +1,28 @@
-function TransactionsHistoryNav() {
-  return <div></div>;
-}
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./TransactionsHistoryNav.module.css";
+
+const TransactionsHistoryNav = () => {
+  return (
+    <nav className={styles.nav}>
+      <NavLink
+        to="/transactions/history/expenses"
+        className={({ isActive }) =>
+          isActive ? `${styles.link} ${styles.activeLink}` : styles.link
+        }
+      >
+        All Expense
+      </NavLink>
+      <NavLink
+        to="/transactions/history/incomes"
+        className={({ isActive }) =>
+          isActive ? `${styles.link} ${styles.activeLink}` : styles.link
+        }
+      >
+        All Income
+      </NavLink>
+    </nav>
+  );
+};
 
 export default TransactionsHistoryNav;
