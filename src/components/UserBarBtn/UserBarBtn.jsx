e89fb5b56f7ede6183ecb584e35ecb42f4a47744
Roman Serdiuk <roman.srdk21@gmail.com>
2025-07-04 19:25:39 -0400

    .

--- src/components/UserBarBtn/UserBarBtn.jsx
+++ src/components/UserBarBtn/UserBarBtn.jsx
@@ -45,8 +45,8 @@ const UserBarBtn = ({ onOpenModal, onOpenLogoutModal }) => {
             alt="user avatar"
           />
         ) : (
-          <span className={css.userBarDefAvatar}>{userSelectInfo.name}</span>
-//           <span className={css.userBarDefAvatar}>{userName[0]}</span>
+          // <span className={css.userBarDefAvatar}>{userSelectInfo.name}</span>
+          <span className={css.userBarDefAvatar}>{userName[0]}</span>
         )}
         <span className={css.userBarBtnName}>{userName}</span>
         <span className={css.userBarBtnIconWrap}>
