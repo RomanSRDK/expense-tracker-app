35343ed4a1df8a7af5e31ae8dfe2ba5b157d6001
Hanna Ponomarenko <pnmrnk.anna@gmail.com>
2025-07-05 16:15:23 +0200

    add BurgerMenu  (#35)
    * add burger btn
    
    * update styles
    
    * add UserBarBtn and UserPanel
    
    * fix UserPanel
    
    * changes
    
    * update UserBarBtn ans fix styles
    
    * add BurgerMenu
    
    * some fix

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,5 +1,57 @@
-function BurgerMenu() {
-  return <div>BurgerMenu</div>;
-}
+import { useEffect } from "react";
+import TransactionsHistoryNav from "../TransactionsHistoryNav/TransactionsHistoryNav";
+import UserBarBtn from "../UserBarBtn/UserBarBtn";
+import { RiCloseLargeLine } from "react-icons/ri";
+import clsx from "clsx";
+import style from "./BurgerMenu.module.css";
+
+const BurgerMenu = ({
+  onOpenModal,
+  onClose,
+  isBurgerOpen,
+  onOpenLogoutModal,
+}) => {
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    if (isBurgerOpen) {
+      window.addEventListener("keydown", handleEsc);
+    }
+
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, [isBurgerOpen, onClose]);
+
+  return (
+    <div
+      className={clsx(
+        style.burgerMenuBackdrop,
+        isBurgerOpen && style.burgerMenuOpen
+      )}
+      onClick={(e) => e.target === e.currentTarget && onClose()}
+    >
+      <div className={style.burgerMenu}>
+        <div className={style.burgerCloseUserBtnBar}>
+          <UserBarBtn
+            onOpenModal={() => {
+              onOpenModal();
+              onClose();
+            }}
+            onOpenLogoutModal={onOpenLogoutModal}
+          />
+          <button onClick={onClose} className={style.closeBtn}>
+            <RiCloseLargeLine className={style.closeBtnIcon} />
+          </button>
+        </div>
+        <div className={style.transactionsHistoryNavWrap}>
+          <TransactionsHistoryNav onNavigate={onClose} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default BurgerMenu;
