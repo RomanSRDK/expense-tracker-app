6969037d2de22109f6dfce712573686258597d06
ivannakm <ivadatalover@gmail.com>
2025-07-07 20:20:40 +0200

    fixed BgImageWrapper and WelcomePage positions

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,6 +3,7 @@ import BgImageWrapper from "../BgImageWrapper/BgImageWrapper";
 import Container from "../Container/Container";
 import Section from "../Section/Section";
 import s from "./SharedLayout.module.css";
+import { useEffect, useState } from "react";
 
 const SharedLayout = () => {
   const location = useLocation();
@@ -11,16 +12,40 @@ const SharedLayout = () => {
     location.pathname === "/login" ||
     location.pathname === "/register" ||
     location.pathname === "/";
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isDesktop = windowWidth >= 1440;
   return (
     <Container>
       <Section>
         <div className={`${s.layoutWrapper} ${isAuthPage ? s.authLayout : ""}`}>
-          <div className={s.imageContainer}>
-            <BgImageWrapper />
-          </div>
-          <main className={s.main}>
-            <Outlet />
-          </main>
+          {/* Conditional rendering order */}
+          {isDesktop ? (
+            <>
+              <div className={s.imageContainer}>
+                <BgImageWrapper />
+              </div>
+              <main className={s.main}>
+                <Outlet />
+              </main>
+            </>
+          ) : (
+            <>
+              <main className={s.main}>
+                <Outlet />
+              </main>
+              <div className={s.imageContainer}>
+                <BgImageWrapper />
+              </div>
+            </>
+          )}
         </div>
       </Section>
     </Container>
