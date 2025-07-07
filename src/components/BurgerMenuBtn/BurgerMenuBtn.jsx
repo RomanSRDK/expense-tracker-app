76b2c39c5027fb1882ff3bcef946ed89cd1db796
illia-dp <illiachernysh.dp@gmail.com>
2025-07-07 09:21:30 +0300

    Fix Time Input (#53)
    * WIP
    
    * add restricted and private toute, edit header
    
    * edit width for info
    
    * add currency in transaction form, bug fix in categories modal
    
    * bug fix with currency
    
    * edit navigation on pages
    
    * start refactoring transaction form to universal component
    
    * Part of the work with editing a transaction
    
    * Part of the work with editing a transaction
    
    * WIP styles
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Edit Time input
    
    * go to sleep!
    
    ---------
    
    Co-authored-by: Roman Serdiuk <100283300+RomanSRDK@users.noreply.github.com>

--- src/components/BurgerMenuBtn/BurgerMenuBtn.jsx
+++ src/components/BurgerMenuBtn/BurgerMenuBtn.jsx
@@ -1,9 +1,10 @@
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import style from "./BurgerMenuBtn.module.css";
 
-const BurgerMenuBtn = ({ onClick }) => {
+const BurgerMenuBtn = () => {
+  const handleClick = () => {};
   return (
-    <button type="button" className={style.burgerMenuBtn} onClick={onClick}>
+    <button type="button" className={style.burgerMenuBtn} onClick={handleClick}>
       <HiOutlineMenuAlt4 className={style.burgerMenuIcon} />
     </button>
   );
